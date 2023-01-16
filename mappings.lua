1d2e3bd444a8daa2d2c4bfb30adb2073a2b7f204
Alex March <alexmarch@fastmail.com>
2023-01-16 19:42:24 +0900

    fix: add extra mappings

--- mappings.lua
+++ mappings.lua
@@ -8,15 +8,17 @@ return {
 
     -- mappings seen under group name "Buffer"
     -- ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    -- ["<leader>bc"] = { "<cmd>BufferLinePickClose<cr>", desc = "Pick to close" },
-    -- ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
-    -- ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
 
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["J"] = { "mzJ`z", desc = "Join lines" },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+  },
+  v = {
+    ["J"] = { ":m '>+1<cr>gv=gv", desc = "Move selection up" },
+    ["K"] = { ":m '<-2<cr>gv=gv", desc = "Move selection down" },
   },
 }

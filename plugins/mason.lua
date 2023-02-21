c34f53052ff13497d40adc9b4f8e4b48e6cab7c2
Alex March <alexmarch@fastmail.com>
2023-02-21 15:45:28 +0900

    feat: add bash lsp

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -3,20 +3,20 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = { "lua_ls", "pyright", "rust_analyzer", "yamlls" },
+      ensure_installed = { "lua_ls", "pyright", "rust_analyzer", "yamlls", "marksman" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
     opts = {
-      ensure_installed = { "stylua", "black" },
+      ensure_installed = { "stylua", "black", "shellcheck", "shfmt" },
     },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     opts = {
-      ensure_installed = { "python" },
+      ensure_installed = { "bash", "python" },
     },
   },
 }

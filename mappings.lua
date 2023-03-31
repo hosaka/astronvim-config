6e9b164d2bcf56dfdc8c44c00fdfc915ed4f5c43
Alex March <alexmarch@fastmail.com>
2023-03-31 21:55:21 +0900

    fix: astronvim core utils require changed in v3

--- mappings.lua
+++ mappings.lua
@@ -12,11 +12,11 @@ return {
     ["]b"] = false,
     ["[b"] = false,
     ["<S-l>"] = {
-      function() require("core.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
       desc = "Next buffer",
     },
     ["<S-h>"] = {
-      function() require("core.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      function() require("astronvim.utils.buffer").nav( -(vim.v.count > 0 and vim.v.count or 1)) end,
       desc = "Previous buffer",
     },
 

1fc0aec68a20c0d02312a1859632fe3787433040
Alex March <alexmarch@fastmail.com>
2023-06-30 15:49:32 +0900

    fix: disable catppuccin for now

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,6 +1,5 @@
 return function()
   return {
-    CursorLineFold = { link = "CursorLineNr" }, -- highlight fold indicator as well as line number
     HighlightURL = { underline = true }, -- always underline URLs
   }
 end

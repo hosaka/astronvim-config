2b3a2f0a29afc206e161eb3b46cc74b727a3285e
Alex March <alexmarch@fastmail.com>
2023-07-21 13:48:10 +0900

    feat: add clipboard handling for wsl env

--- polish.lua
+++ polish.lua
@@ -1,6 +1,3 @@
--- This function is run last and is a good place to configuring
--- augroups/autocommands and custom filetypes also this just pure lua so
--- anything that doesn't fit in the normal config locations above can go here
 return function()
   -- Set up custom filetypes
   -- vim.filetype.add {
@@ -15,5 +12,19 @@ return function()
   --   },
   -- }
   require "user.autocmds"
-end
 
+  if vim.fn.has "wsl" then
+    vim.g.clipboard = {
+      name = "WslClipboard",
+      copy = {
+        ["+"] = "clip.exe",
+        ["*"] = "clip.exe",
+      },
+      paste = {
+        ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r",""))',
+        ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r",""))',
+      },
+      cache_enabled = 0,
+    }
+  end
+end

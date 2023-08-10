5488886231d395251c87691e42b92d855c0ad36a
Alex March <alexmarch@fastmail.com>
2023-08-10 12:09:13 +0900

    feat: add rust-tools and configure rust-analyzer

--- lsp/setup_handlers.lua
+++ lsp/setup_handlers.lua
@@ -3,8 +3,12 @@ return {
   -- first function changes the default setup handler
   function(server, opts) require("lspconfig")[server].setup(opts) end,
   -- keys for a specific server name will be used for that LSP
-  sumneko_lua = function(server, opts)
+  sumneko_lua = function(_, opts)
     -- custom sumneko_lua setup handler
     require("lspconfig")["sumneko_lua"].setup(opts)
+  end,
+  rust_analyzer = function(_, opts)
+    -- custom rust_analyzer setup handler
+    require("rust-tools").setup { server = opts }
   end,
 }
